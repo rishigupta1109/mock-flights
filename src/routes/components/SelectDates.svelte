1a203c762979cd67ff16fd0dec8c88023c1e725f
Rishi <grishi634@gmail.com>
2024-02-08 17:33:48 +0530

    implemented svelte calendar

--- src/routes/components/SelectDates.svelte
+++ src/routes/components/SelectDates.svelte
@@ -1,22 +1,67 @@
-<script>
+<script lang="ts">
 	import DuoCard from '$lib/components/DuoCard.svelte';
 	import AddIcon from '$lib/icons/addIcon.svelte';
+	import { Datepicker } from 'svelte-calendar';
+	import dayjs from 'dayjs';
+
+	let departureDate: any;
+	let returnDate: any;
+
+	let theme = {
+		calendar: {
+			height: '100vh'
+		}
+	};
 </script>
 
 <DuoCard>
-	<div slot="left" class="flex w-[45%] flex-col p-2">
-		<span class="card-content base-content-light">Departure</span>
-		<span class="card-sub-heading">7 feb</span>
-		<span class="card-content base-content-light">Wednesday</span>
+	<div slot="left" class="flex w-[45%] flex-col p-2 cursor-pointer">
+		<Datepicker bind:store={departureDate} let:key let:send let:receive {theme}>
+			<button class="flex flex-col w-full" in:receive|local={{ key }} out:send|local={{ key }}>
+				{#if $departureDate?.hasChosen}
+					<span class="card-content base-content-light">Departure</span>
+					<span class="card-sub-heading">{dayjs($departureDate.selected).format('D MMM')}</span>
+					<span class="card-content base-content-light">
+						{dayjs($departureDate.selected).format('dddd')}
+					</span>
+				{:else}
+					<span class="card-content base-content-light">Departure</span>
+					<span class="card-sub-heading">Select Date</span>
+					<span class="card-content base-content-light"></span>
+				{/if}
+			</button>
+		</Datepicker>
 	</div>
 	<div slot="right" class="flex w-[45%] justify-between items-center">
-		<div class="flex flex-col p-2">
+		<Datepicker bind:store={returnDate} let:key let:send let:receive {theme}>
+			<button class="flex flex-col w-full" in:receive|local={{ key }} out:send|local={{ key }}>
+				{#if $returnDate?.hasChosen}
+					<span class="card-content base-content-light">Departure</span>
+					<span class="card-sub-heading">{dayjs($returnDate.selected).format('D MMM')}</span>
+					<span class="card-content base-content-light">
+						{dayjs($returnDate.selected).format('dddd')}
+					</span>
+				{:else}
+					<div class="flex justify-between items-center w-full gap-8">
+						<div class="flex flex-col justify-center items-start">
+							<span class="card-content base-content-light">Return</span>
+							<span class="card-sub-heading">Add Return</span>
+							<span class="card-content base-content-light"> and save more! </span>
+						</div>
+						<button class="h-6 aspect-square rounded-full cursor-pointer">
+							<AddIcon color="primary" testId="add-icon" />
+						</button>
+					</div>
+				{/if}
+			</button>
+		</Datepicker>
+		<!-- <div class="flex flex-col p-2">
 			<span class="card-content base-content-light">Return</span>
 			<span class="card-sub-heading">Add Return</span>
 			<span class="card-content base-content-light">and save more!</span>
 		</div>
 		<button class="h-6 aspect-square rounded-full cursor-pointer">
 			<AddIcon color="primary" testId="add-icon" />
-		</button>
+		</button> -->
 	</div>
 </DuoCard>
