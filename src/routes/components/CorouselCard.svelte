4ba8a84e0d765e1438842531e85ad026a380286a
Rishi <grishi634@gmail.com>
2024-02-08 15:58:17 +0530

    fixed height of arrow icon

--- src/routes/components/CorouselCard.svelte
+++ src/routes/components/CorouselCard.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="flex justify-between items-center min-w-80 p-4 gap-8 rounded-md bg-base-200">
-	<div class="flex justify-between items-center gap-4">
+	<div class="flex justify-between items-center gap-4 w-9/10">
 		<span class="w-2/5 flex justify-center items-center">
 			{#if isRecent}
 				<svelte:component this={item.icon} class="h-14" />
@@ -17,7 +17,7 @@
 		<div class="flex w-3/5 flex-col text-black">
 			<p class="flex items-center w-full whitespace-nowrap gap-1">
 				{item.source}
-				<RightArrow />
+				<RightArrow color="black" testId="arrow" />
 				{item.destination}
 			</p>
 			<p class="text-xs base-content-light">{item.date}</p>
